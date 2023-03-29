9427b56e6198de83a8f01eaffc156268015d8de9
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-03-28 21:18:18 -0300

    uso useSelector y useDispatch

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 
-
+import { useSelector, useDispatch } from "react-redux"
+import { setUser } from "./reducers/user/userSlice";
 
 export const  App =() => {
   
-
+  const dispatch = useDispatch();
+  const { email } = useSelector(state => state.user); 
   return (
    
       <div>
       <h1> Hello, world!</h1>
-      <p> This is a simple react app </p>
+      <p> This is a simple react app  email {email}</p>
+      <button className="btn-btn-primary"
+      onClick={ ()=>{
+        dispatch(setUser({
+          email: "prueba@gmail.com",
+          fullname: "Noelia",
+          token: "123456"
+        }))
+      }}
+      > Cambiar User </button>
     </div>
   )
 }
